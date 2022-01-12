e24c82f341f95419de8bbb328820851e85e6b85f
Shreyans Shah <shrey@mbp14inch2021.myfiosgateway.com>
2022-01-11 21:33:48 -0500

    Learning Api calls and passing props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,20 +7,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hello Shreyans",
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "Hello Namit" })}>
-            Change Text
-          </button>
-        </header>
+        <CardList name="shreyans">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+        </CardList> 
       </div>
     );
   }
