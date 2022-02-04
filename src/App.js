f6cef9ab838578adcc5fcfc211ea2a6e3c561f4a
Shreyans Shah <shrey@shreyanss-mbp.myfiosgateway.com>
2022-02-03 21:39:24 -0500

    Finishing touch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -20,15 +19,21 @@ class App extends Component {
   }
 
   handleChange = (e) => {
-    this.setState({ searchField: e.target.value })
-  }
+    this.setState({ searchField: e.target.value });
+  };
 
   render() {
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()));
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
     return (
       <div className="App">
-        <SearchBox placeholder='search monsters' handleChange={e => this.handleChange(e)} />
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(e) => this.handleChange(e)}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
