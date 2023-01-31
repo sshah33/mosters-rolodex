b569247f85b0d6fa270c0c4c79fd4a6f87089268
sshah33 <shrey@Shreyanss-MacBook-Pro.local>
2023-01-31 18:43:47 -0500

    final changes

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./card.styles.css";
 
 export const Card = ({ monster }) => {
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./search-box.styles.css";
 
 export const SearchBox = ({ placeholder, onChangeHandler, className }) => (
